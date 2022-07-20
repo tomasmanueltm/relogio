3cb640edf919245278a2296f058954545b8600d2
TomasManueltm <antoniomanuelbaptistabaptista@gmail.com>
2022-07-20 23:31:14 +0100

    reajuste js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-// "use strict";
+"use strict";
 
 let JS = (e)=> document.querySelector(e),
 hoje = new Date(),
@@ -11,25 +11,24 @@ var diaSemana = hoje.toUTCString().split(",")[0],
 
 
 
-
+// percorrer os dias de semana
 dias.forEach(element => {
-    if(element.dataset.dia == diaSemana.toString())
-    {
-        element.classList.add("dia");
-    }
+    (element.dataset.dia == diaSemana.toString()) ? element.classList.add("dia") : null;  
 });
-// 
+
+// adiciona valores nas label
 function relogio(){
-    hoje = new Date()
-    hora.innerText = hoje.getDate();
+    hoje = new Date();
+    hora.innerText = hoje.getHours();
     minuto.innerText = hoje.getUTCMinutes();
     segundo.innerText = hoje.getSeconds();
 };
+
 // atualizacao
 setInterval(() => {
     relogio();
 
-    hora.innerText = (hora.textContent.length == 1) ? "0"+hoje.getDate() : hoje.getDate();
+    hora.innerText = (hora.textContent.length == 1) ? "0"+hoje.getHours() : hoje.getHours();
     minuto.innerText = (minuto.textContent.length == 1) ? "0"+hoje.getUTCMinutes() : hoje.getUTCMinutes();
     segundo.innerText = (segundo.textContent.length == 1) ? "0"+hoje.getSeconds() : hoje.getSeconds();
 }, 100);
